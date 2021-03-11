6dcdf51a4ffa321449b9ac380c3a2c22d066bc2f
tostos <saittosun2@gmail.com>
2021-03-11 13:32:57 +0100

    second commit

--- src/app/customers/customers.component.ts
+++ src/app/customers/customers.component.ts
@@ -1,15 +1,28 @@
-import { Component, OnInit } from '@angular/core';
+import { CustomerDetail } from './../models/customer-detail';
+import { Component, OnInit, OnDestroy } from '@angular/core';
+import { Subject } from 'rxjs';
+import { CustomerService } from '../core/services/customer.service';
 
 @Component({
   selector: 'app-customers',
   templateUrl: './customers.component.html',
   styleUrls: ['./customers.component.scss']
 })
-export class CustomersComponent implements OnInit {
+export class CustomersComponent implements OnInit, OnDestroy {
 
-  constructor() { }
+  customers: CustomerDetail[] = [];
+  private destroyed$ = new Subject<boolean>();
+
+  constructor(private customerService: CustomerService) { }
 
   ngOnInit(): void {
+   this.customerService.getData()
+
+  }
+
+  ngOnDestroy(): void {
+    this.destroyed$.next(true);
+    this.destroyed$.complete();
   }
 
 }
