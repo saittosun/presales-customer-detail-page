0bac9efbd353888721d6f269bc834ef0a28b62d0
tostos <saittosun2@gmail.com>
2021-03-12 20:28:17 +0100

    dagistan bitti

--- src/app/models/customer.ts
+++ src/app/models/customer.ts
@@ -5,3 +5,25 @@ export interface Customer {
   status: string,
   date: string
 }
+
+// export interface Customer {
+//   id: number,
+//   company: string,
+//   projectName: string,
+//   projectStatus: string,
+//   deadline: string,
+//   firstName: string,
+//   lastName: string,
+//   email: string,
+//   phonenumber: number,
+//   vat: number,
+//   address: Address
+// }
+
+// export interface Address {
+//   addressline: string,
+//   city: string,
+//   country: string,
+//   state: string,
+//   zip: string,
+// }
